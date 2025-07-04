aad1e95dcbe277c7a3fe50d7dee496a228111d71
yadar <yadar@sai>
2025-07-04 21:31:45 +0530

    hi

---
+++ OrangeHRM/src/testcases/Test2.java
@@ -0,0 +1,18 @@
+package testcases;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+	
+		
+		
+		for (int j = 0; j < 10; j++) {
+			
+			j=j+2;
+			System.out.println(j);
+			
+		} System.out.println("cat");
+
+	}
+
+}
---
+++ OrangeHRM/src/testcases/Testcase1.java
@@ -0,0 +1,13 @@
+package testcases;
+
+public class Testcase1 {
+
+	public static void main(String[] args) {
+		
+		webdriver driver=new webdriver
+				
+		
+		System.out.println("This is testcase1");
+	}
+
+}
